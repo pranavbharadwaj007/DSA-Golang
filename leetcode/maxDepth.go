431c84737569cacc3fd39e33dc4ba91170c2839f
Pranav N <pranavbharadwaj142@gmail.com>
2023-10-05 21:47:17 +0530

    Create maxDepth.go

---
+++ leetcode/maxDepth.go
@@ -0,0 +1,12 @@
+func maxDepth(root *TreeNode) int {
+    if root == nil{
+        return 0
+    }
+    left := 1+ maxDepth(root.Left)
+    right := 1+  maxDepth(root.Right)
+
+    if left > right {
+        return left
+    }
+    return right
+}
